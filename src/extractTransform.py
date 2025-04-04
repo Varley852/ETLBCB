5fbadd408acfdcf59a25a24e0f8602a9e8a3efb7
Varley Marques <163340230+Varley852@users.noreply.github.com>
2025-04-04 15:55:35 -0300

    aula 03

--- src/extractTransform.py
+++ src/extractTransform.py
@@ -1,14 +1,15 @@
 import requests
 import pandas as pd
 
+
 def requestApiBcb(data: str) -> pd.DataFrame:
     """
     Função para extrair os dados da API do Banco Central.
     """
-    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json" 
-    req = requests.get(url) 
+    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json"
+    req = requests.get(url)
     print("Status Code:", req.status_code)
-    data = req.json() 
+    data = req.json()
 
     df = pd.json_normalize(data["value"])
     df["datatrimestre"] = pd.to_datetime(df["datatrimestre"])
